46dcff99d57e79fc2e9c7069f7a88a3bbcda6888
Julien Claivaz <julienclaivaz@juliens-mbp.home>
2023-01-24 23:27:22 +0100

    loop for hours of the day

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-let $textArea = $("<textarea>");
-let $button = $("<button>");
-let $hour = $("<span>")
+let $ul = $("<ul>")
 
-$(".container").append($hour);
-$(".container").append($textArea);
-$(".container").append($button);
+
+
+
+let hoursDay = ["9 am", "10am", "11am", "12pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"]
+
+
+$(".container").append($ul)
+$(".container").addClass("time-block");
+
+
+
+for (let i = 0; i < hoursDay.length; i++) {
+
+    let $li = $("<li>")
+    let $textArea = $("<textarea>");
+    let $button = $("<button>");
+    let $hour = $("<span>")
+
+$ul.append($li)
+$li.append($hour);
+$li.append($textArea);
+$li.append($button);
+
+$hour.text(hoursDay[i]);
+
+$li.addClass("row")
+$hour.addClass("hour");
+$button.addClass("saveBtn");
+$button.attr("id", hoursDay[i])
+$textArea.attr("id", hoursDay[i])
 
 $button.text("submit");
-$hour.text("9am");
 
-$hour.addClass("hour");
-$(".container").addClass("time-block");
-$button.addClass("saveBtn");
+$("button").on("click", function(event){
+
+    let meeting = $(event.target).siblings("textarea").val();
+    localStorage.setItem(hoursDay[i], meeting)
+})
+
+$textArea.text(localStorage.getItem(hoursDay[i]))
+
+
+}
 
 
