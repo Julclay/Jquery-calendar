d6b4e6111e203e1ba8df2d9f936364ddd7a7d428
Julien Claivaz <julienclaivaz@juliens-mbp.home>
2023-01-26 18:23:39 +0100

    local storage working

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let $ul = $("<ul>")
 
+let today = moment();
+$("#currentDay").text(today.format("D MMM YYYY"));
 
+let time = moment().format("H");
 
+console.log(time);
 
-let hoursDay = ["9 am", "10am", "11am", "12pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"]
-
+let hoursDay = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
 $(".container").append($ul)
 $(".container").addClass("time-block");
-
-
 
 for (let i = 0; i < hoursDay.length; i++) {
 
@@ -23,25 +24,53 @@ $li.append($hour);
 $li.append($textArea);
 $li.append($button);
 
-$hour.text(hoursDay[i]);
+$hour.text(hoursDay[i] + " h");
 
 $li.addClass("row")
 $hour.addClass("hour");
 $button.addClass("saveBtn");
 $button.attr("id", hoursDay[i])
-$textArea.attr("id", hoursDay[i])
 
 $button.text("submit");
 
-$("button").on("click", function(event){
+$(`#${hoursDay[i]}`).on("click", function(event){
 
     let meeting = $(event.target).siblings("textarea").val();
     localStorage.setItem(hoursDay[i], meeting)
+
+
+
+    
 })
 
 $textArea.text(localStorage.getItem(hoursDay[i]))
 
 
 }
+// 
+
+let $textArea = $("<textarea>");
+
+let buttonIda = document.querySelector(".saveBtn")
+let buttonId = buttonIda.id
+
+console.log(buttonId)
+
+if(time < buttonId){
+
+    $textArea.css("background-color", "red")
+    console.log("red")
+
+}else if (time > buttonId){
+
+    $textArea.css("background-color", "blue")
+    console.log("blue")
+
+}else if (time === buttonId){
+
+    $textArea.css("background-color", "grey")
+    console.log("grey")
+}
+
 
 
